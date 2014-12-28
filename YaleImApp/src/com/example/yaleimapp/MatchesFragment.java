1ef1c1d17e111f80d4121fa400eb1d6e81d562a8
Andrew Malta <andrew.malta@yale.edu>
2014-12-27 23:34:49 -0800

    encapsulates getMatches function

--- YaleImApp/src/com/example/yaleimapp/MatchesFragment.java
+++ YaleImApp/src/com/example/yaleimapp/MatchesFragment.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class MatchesFragment extends ListFragment{
+	final Match[] matches = getMatches();
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		return inflater.inflate(R.layout.matches_list_fragment, container, false);
@@ -22,15 +23,35 @@ public class MatchesFragment extends ListFragment{
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 	    super.onActivityCreated(savedInstanceState);
-        final Match[] matches = {new Match(new ResidentialCollege("Berkeley", R.drawable.berkeley),
-        		                           new ResidentialCollege("SayBrook", R.drawable.saybrook),
-        		                           new Date(), "Soccer", "IM Fields"),
-        		                 new Match(new ResidentialCollege("Erza Stiles", R.drawable.erzastiles),
-      		                               new ResidentialCollege("Johnathan Edwards", R.drawable.johnathanedwards),
-      		                               new Date(), "Soccer", "IM Fields") };
+       
 	    
 	    ListAdapter adapter = new MatchesAdapter(getActivity(), matches);
 	    setListAdapter(adapter);  
+	}
+	
+	//dummy encapsulated method that retrieves the matches list for the matches tab.
+	public Match[] getMatches(){
+		Match[] matches = {new Match(new ResidentialCollege("Berkeley", R.drawable.berkeley),
+                new ResidentialCollege("SayBrook", R.drawable.saybrook),
+                new Date(), "Soccer", "IM Fields"),
+                
+                new Match(new ResidentialCollege("Erza Stiles", R.drawable.erzastiles),
+                new ResidentialCollege("Johnathan Edwards", R.drawable.johnathanedwards),
+                new Date(), "Football", "IM Fields"),
+
+               new Match(new ResidentialCollege("Johnathan Edwards", R.drawable.johnathanedwards),
+               new ResidentialCollege("Trumbull", R.drawable.trumbull),
+               new Date(), "Football", "IM Fields"),
+
+               new Match(new ResidentialCollege("Calhoun", R.drawable.calhoun),
+               new ResidentialCollege("Silliman", R.drawable.silliman),
+               new Date(), "Football", "IM Fields"),
+               
+               new Match(new ResidentialCollege("Branford", R.drawable.branford),
+               new ResidentialCollege("berkeley", R.drawable.berkeley),
+               new Date(), "Football", "IM Fields")};
+		
+		return matches;
 	}
 	
 }
